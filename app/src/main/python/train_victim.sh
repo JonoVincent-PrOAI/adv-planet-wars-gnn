07fd4fe8e2aa4e1becbe7a39aa8adeb771275a1d
JonoVincent-PrOAI <yz24943@bristol.ac.uk>
2026-07-09 11:30:06 +0100

    train script update

--- app/src/main/python/train_victim.sh
+++ app/src/main/python/train_victim.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=train
-#SBATCH --output=train.out
+#SBATCH --output=selfplay.out
 #SBATCH --gpus=1
-#SBATCH --cpus-per-gpus=60
+#SBATCH --cpus-per-gpu=60
 #SBATCH --ntasks-per-gpu=1
-#SBATCH --time=24:00:00  
-
-module load cuda/12.6
+#SBATCH --time=24:00:00
 
 source ~/miniforge3/bin/activate
 conda activate .planetWarsVenv
+module load cuda/12.6
 
 python agents/ppo.py
